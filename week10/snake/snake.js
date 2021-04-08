c0f77578a8db46d7bacf11890d7e9e28c0efb99a
Sophie_Z <68120193+Sophie-Xinyu-Zhang@users.noreply.github.com>
2021-04-08 16:58:19 -0400

    1

---
+++ week10/snake/snake.js
@@ -0,0 +1,142 @@
+$(document).ready(function () {
+  // ---------------
+  // INITIALIZATIONS
+  // ---------------
+  let canvas = $("#myCanvas")[0];
+  let ctx = canvas.getContext("2d");
+  let width = $("#myCanvas").width();
+  let height = $("#myCanvas").height();
+  let cw = 15;
+  let food;
+  let snake;
+  let direction = "RIGHT";
+
+  // --------------
+  // MAIN FUNCTIONS
+  // --------------
+
+  // update every .1 second to keep the game playing, or
+  // update the position of the player and see if need to
+  // re-generate the canvas/food/snake
+  let gameloop = setInterval(update, 100);
+
+  // initialization of the snake and the food using functions
+  // defined below
+  placeFood();
+  paint_cell(food.x, food.y, "grey");
+  placeSnake();
+  paint_cell(snake.x, snake.y, "green");
+
+  // function that updates the current status of player
+  // if the player hit the border
+  // if the player got the food
+  // re-generate the canvas and the player and the food
+
+  // the main function that keeps the game going
+  function update() {
+    if (direction == "RIGHT") snake.x = snake.x + 1;
+    else if (direction == "LEFT") snake.x = snake.x - 1;
+    else if (direction == "UP") snake.y = snake.y - 1;
+    else if (direction == "DOWN") snake.y = snake.y + 1;
+
+    check_borders();
+    check_food();
+    blank();
+    paint_cell(food.x, food.y, "grey");
+    paint_cell(snake.x, snake.y, "green");
+  }
+
+  // ----------------
+  // HELPER FUNCTIONS
+  // ----------------
+
+  // when the player hits the wall, clearInterval here
+  function showGameOver() {
+    let current = $("#current").text();
+    $("#final").text(current);
+    $("#gameover").fadeIn();
+  }
+
+  // randomly generate a food on the canvas
+  function placeFood() {
+    food = {
+      x: Math.round((Math.random() * (width - cw)) / cw),
+      y: Math.round((Math.random() * (height - cw)) / cw),
+    };
+  }
+
+  // place the snake at the start of the game randomly
+  function placeSnake() {
+    snake = {
+      x: Math.round((Math.random() * (width - cw)) / cw),
+      y: Math.round((Math.random() * (height - cw)) / cw),
+    };
+  }
+
+  // paint the canvas
+  function blank() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, width, height);
+    ctx.strokeStyle = "white";
+    ctx.strokeRect(0, 0, width, height);
+  }
+
+  // check if the player hits the border
+  function check_borders() {
+    if (
+      snake.x < 0 ||
+      snake.x > (width - cw) / cw ||
+      snake.y < 0 ||
+      snake.y > (height - cw) / cw
+    ) {
+      clearInterval(gameloop);
+      showGameOver();
+    }
+  }
+
+  // generic function to paint the cell on the canvas to be either
+  // the food or the snake depending on the input
+  function paint_cell(x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x * cw, y * cw, cw, cw);
+    ctx.strokeStyle = "white";
+    ctx.strokeRect(x * cw, y * cw, cw, cw);
+  }
+
+  // check if the player hits the food, if so, update current score
+  function check_food() {
+    if (food.x == snake.x && food.y == snake.y) {
+      let current = parseInt($("#current").text());
+      current += 1;
+      $("#current").text(current);
+      placeFood();
+    }
+  }
+
+  // read in which direction the player is going / which key
+  // is being pressed
+  // after detecting the key, also check if the player hits the
+  // food, if so, update accordingly as did above
+  $(document).keydown(function (event) {
+    let key = event.which;
+    if (key == "37") {
+      snake.x -= 1;
+      direction = "LEFT";
+    } else if (key == "38") {
+      snake.y -= 1;
+      direction = "UP";
+    } else if (key == "39") {
+      snake.x += 1;
+      direction = "RIGHT";
+    } else if (key == "40") {
+      snake.y += 1;
+      direction = "DOWN";
+    }
+    check_food();
+    blank();
+    let color = "grey";
+    paint_cell(food.x, food.y, color);
+    color = "green";
+    paint_cell(snake.x, snake.y, color);
+  });
+});
