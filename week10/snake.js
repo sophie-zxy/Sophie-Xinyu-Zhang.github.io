1dc24381798485ff9e4b7b2d668c172bc367fe10
Sophie_Z <68120193+Sophie-Xinyu-Zhang@users.noreply.github.com>
2021-04-13 14:32:55 -0400

    1

--- week10/snake.js
+++ week10/snake.js
@@ -10,7 +10,31 @@ $(document).ready(function () {
   let food;
   let snake;
   let direction = "RIGHT";
-
+  let ingame = false
+  let name;
+  
+  // button to get input from user
+  var button = document.getElementById("b1");
+    button.addEventListener("click", function(){
+        name = document.getElementById("input").value;
+        document.getElementById("input").value = "";
+        ingame = true;
+        $("#type").hide();
+        $("#hi").text(name + "'s");
+    })
+    
+    //Re-play game button
+    var button2 = document.getElementById("ha");
+    button2.addEventListener("click", function(){
+        ingame = false;
+        placeFood();
+        placeSnake();
+        $("#gameover").hide();
+        $("#type").show();
+        $("#current").text(0);
+    })
+    
+    
   // --------------
   // MAIN FUNCTIONS
   // --------------
@@ -23,7 +47,7 @@ $(document).ready(function () {
   // initialization of the snake and the food using functions
   // defined below
   placeFood();
-  paint_cell(food.x, food.y, "grey");
+  paint_cell(food.x, food.y, "#abdbe3");
   placeSnake();
   paint_cell(snake.x, snake.y, "green");
 
@@ -33,17 +57,20 @@ $(document).ready(function () {
   // re-generate the canvas and the player and the food
 
   // the main function that keeps the game going
-  function update() {
-    if (direction == "RIGHT") snake.x = snake.x + 1;
-    else if (direction == "LEFT") snake.x = snake.x - 1;
-    else if (direction == "UP") snake.y = snake.y - 1;
-    else if (direction == "DOWN") snake.y = snake.y + 1;
 
-    check_borders();
-    check_food();
-    blank();
-    paint_cell(food.x, food.y, "grey");
-    paint_cell(snake.x, snake.y, "green");
+  function update() {
+      if (ingame){
+        if (direction == "RIGHT") snake.x = snake.x + 1;
+        else if (direction == "LEFT") snake.x = snake.x - 1;
+        else if (direction == "UP") snake.y = snake.y - 1;
+        else if (direction == "DOWN") snake.y = snake.y + 1;
+
+        check_borders();
+        check_food();
+        blank();
+        paint_cell(food.x, food.y, "abdbe3");
+        paint_cell(snake.x, snake.y, "green");
+      }
   }
 
   // ----------------
@@ -52,10 +79,12 @@ $(document).ready(function () {
 
   // when the player hits the wall, clearInterval here
   function showGameOver() {
+    ingame = false;
     let current = $("#current").text();
-    $("#final").text(current);
+    $("#myfinal").text(current);
     $("#gameover").fadeIn();
   }
+
 
   // randomly generate a food on the canvas
   function placeFood() {
@@ -75,10 +104,8 @@ $(document).ready(function () {
 
   // paint the canvas
   function blank() {
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "#Dbf5df";
     ctx.fillRect(0, 0, width, height);
-    ctx.strokeStyle = "white";
-    ctx.strokeRect(0, 0, width, height);
   }
 
   // check if the player hits the border
@@ -89,7 +116,12 @@ $(document).ready(function () {
       snake.y < 0 ||
       snake.y > (height - cw) / cw
     ) {
-      clearInterval(gameloop);
+//      clearInterval(gameloop) and append item to list; 
+        var item = document.createElement("li");
+        let current = $("#current").text();
+
+        item.append(name + ": " + current);
+        document.getElementById("list").append(item);
       showGameOver();
     }
   }
@@ -119,24 +151,27 @@ $(document).ready(function () {
   // food, if so, update accordingly as did above
   $(document).keydown(function (event) {
     let key = event.which;
-    if (key == "37") {
-      snake.x -= 1;
-      direction = "LEFT";
-    } else if (key == "38") {
-      snake.y -= 1;
-      direction = "UP";
-    } else if (key == "39") {
-      snake.x += 1;
-      direction = "RIGHT";
-    } else if (key == "40") {
-      snake.y += 1;
-      direction = "DOWN";
+    if (ingame){
+        
+        if (key == "37") {
+          snake.x -= 1;
+          direction = "LEFT";
+        } else if (key == "38") {
+          snake.y -= 1;
+          direction = "UP";
+        } else if (key == "39") {
+          snake.x += 1;
+          direction = "RIGHT";
+        } else if (key == "40") {
+          snake.y += 1;
+          direction = "DOWN";
+        }
+        check_food();
+        blank();
+        let color = "grey";
+        paint_cell(food.x, food.y, color);
+        color = "green";
+        paint_cell(snake.x, snake.y, color);
     }
-    check_food();
-    blank();
-    let color = "grey";
-    paint_cell(food.x, food.y, color);
-    color = "green";
-    paint_cell(snake.x, snake.y, color);
   });
 });
